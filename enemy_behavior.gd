28c91c5a3514b2f353526a3182aef4c4d187487e
AnubrataDS <anubrata.stephenite.nrg@gmail.com>
2026-01-19 00:15:59 +0530

    improved swraming

--- enemy_behavior.gd
+++ enemy_behavior.gd
@@ -8,12 +8,13 @@ var velocity: Vector3 = Vector3(0,0,0)
 var seek_strength = Vector3(randf_range(5,10), 0 , randf_range(5,10))
 var push_strength = Vector3(0.9,0.9,0.9)
 var vel_lim = 30
-var proc_lim = 500
+var proc_lim = 20
 var hp = 100
-var target_offset : Vector3
+var hover_radius = randf_range(0.2,1.0)
+var theta = randf_range(0,2*PI)
 func _ready():
-	_update_target_offset()
 	player_ref = get_parent().player_ref
+	_update_target_offset()
 
 func _physics_process(delta: float) -> void:
 	_look()
@@ -25,6 +26,7 @@ func _look() -> void:
 	cone.look_at(player_ref.get_position())
 
 func _move(delta:float) -> void:
+	var target_offset = Vector3(hover_radius*sin(theta),0,hover_radius*cos(theta))
 	if(get_position().distance_to(player_ref.get_position()+target_offset) < 0.1):
 		_update_target_offset()
 	velocity = ((player_ref.get_position()+target_offset) - get_position()).normalized()*seek_strength
@@ -42,7 +44,5 @@ func _updateHpBar() -> void:
 	hpbar.value = hp
 
 func _update_target_offset() -> void:
-	target_offset = Vector3(
-		randf_range(0.5,1)*(-1 if randi_range(0,1)==0 else 1), 
-		0 ,
-		randf_range(0.5,1)*(-1 if randi_range(0,1)==0 else 1))
+	hover_radius = randf_range(0.5,1.5)
+	theta += randf_range(0,PI/10)
